b0f8befe73aa3e34f626443e1f55ef09c5f2065d
Runn1dDefault <runniddefault@gmail.com>
2023-12-16 01:15:35 +0600

    refactoring config and added env-example and README

---
+++ README.md
@@ -0,0 +1,49 @@
+# Authorization service
+
+#### Micro service responsible only for storing and modifying user data, as well as for user authorization and registration
+
+
+Running requires:
+
+* Python 3.11 (tested under Python 3.11.5)
+
+---
+> :required: Create a `.env` file next to `config.py` and `.env-example` and fill it in like the example in `.env-example`
+
+```filepath
+    kaimono_auth  <- Base dir
+    ├── ...
+    ├── api   
+    │   └── ...
+    ├── config.py
+    ├── .env          <- here    
+    ├── .env-example
+    └── ...
+```
+
+**Install dependencies**:
+```bash
+pip install -r requirements.txt
+```
+
+**Run tests**
+> :warning: **in order for the tests to work correctly, redefine USE_TEST to True in the .env file**
+> also after the tests have been checked you will need to return to the False state
+```bash
+python -m tests
+```
+
+**Run falcon app**:
+```bash
+python -m api
+```
+---
+
+### User update operations
+![user update operations jpg](./static/architecture/user_update_operations.jpg)
+
+> - **X-VERIFY-TOKEN** - 
+> This is a token header that is sent to the user's email with url to follow it for further verification of the user,
+> the token itself is intended only for updating the user's password and also for marking the user's email as verified. 
+> If you use this token in the authorization header, an error will be returned,
+> and the authorization token cannot be used in the X-VERIFY-TOKEN header
--- config.py
+++ config.py
@@ -3,26 +3,34 @@ from pathlib import Path
 from decouple import config
 
 
-USE_TEST = True
-TEST_DB_URI = "sqlite:///:memory:"
+SECRET_KEY = config("SECRET_KEY")
+VERIFY_SECRET_KEY = config("VERIFY_SECRET_KEY")
 
+USE_TEST = config("USE_TEST", cast=bool)
+
+# Dirs
 BASE_DIR = Path(__file__).parent
 STATIC_DIR = BASE_DIR / 'static'
 TEMPLATES_DIR = STATIC_DIR / 'templates'
 
+# Postgres urls
 PRIMARY_DB_URI = config("PRIMARY_DB_URI")
+TEST_DB_URI = config("TEST_DB_URI")
+
+# Redis urls
 REDIS_URI = config("REDIS_URI")
 REDIS_TEST_URI = config("REDIS_TEST_URI")
 
-SECRET_KEY = "foo"
-VERIFY_SECRET_KEY = "foo1"
 
+# Validators conf
+PASSWORD_MIN_LEN = 10
+ALLOWED_EMAIL_DOMAINS = ('gmail.com', 'mail.ru')
+
+
+# Auth Token conf
 TOKEN_HASH_ALGORITHM = 'HS256'
 TOKEN_AUTH_HEADER = 'Bearer'
-ALLOWED_EMAIL_DOMAINS = ('gmail.com', 'mail.ru')
-PASSWORD_MIN_LEN = 10
 TOKEN_EXP_SECONDS = 43200
-
 TOKEN_ENCODE_FIELDS_MAP = {
     "user_id": "id",
     "user_email": "email",
@@ -30,16 +38,16 @@ TOKEN_ENCODE_FIELDS_MAP = {
     "user_email_verified": "email_verified"
 }
 
-
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587  # SMTP server port
-SMTP_EMAIL = 'kaimono.sup@gmail.com'
-SMTP_PASSWORD = 'qpxursoeoyhqtrnf'
+# Mailing
+SMTP_SERVER = config("SMTP_SERVER")
+SMTP_PORT = config("SMTP_PORT", cast=int)  # SMTP server port
+SMTP_EMAIL = config("SMTP_EMAIL")
+SMTP_PASSWORD = config("SMTP_PASSWORD")
 VERIFICATION_TTL = 720
 VERIFICATION_CONTEXT = {
     "subject": "Kaimono",
     "warning_text": "if it wasn't you. Please ignore this message and do not share "
                     "the code with anyone."
 }
-REDIRECT_UPDATE_PWD_URL = "http://localhost:3000/update-password/%s"
 VERIFY_EMAIL_REDIRECT_URL = "http://localhost:3000/login"
+REDIRECT_UPDATE_PWD_URL = "http://localhost:3000/update-password/%s"
